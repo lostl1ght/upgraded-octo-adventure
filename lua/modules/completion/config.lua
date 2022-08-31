a06c0e385dda94cf6e1250a6c1cf17167d88c054
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-08-31 04:28:22 +0300

    Add cmp

---
+++ lua/modules/completion/config.lua
@@ -0,0 +1,87 @@
+local config = {}
+
+function config.cmp()
+  local cmp = require('cmp')
+
+  local cmp_buffer = require('cmp_buffer')
+  local luasnip = require('luasnip')
+  vim.o.completeopt = 'menu,menuone,noselect'
+  cmp.setup({
+    mapping = {
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+    },
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    sources = {
+      { name = 'luasnip' },
+      { name = 'nvim_lua' },
+      { name = 'nvim_lsp' },
+      { name = 'path' },
+      {
+        name = 'buffer',
+        keyword_length = 5,
+        option = {
+          keyword_pattern = [[\k\+]],
+        },
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
+    sorting = {
+      comparators = {
+        function(...)
+          return cmp_buffer:compare_locality(...)
+        end,
+      },
+    },
+    formatting = {
+      format = require('lspkind').cmp_format({
+        with_text = true,
+        menu = {
+          buffer = '[buf]',
+          nvim_lsp = '[lsp]',
+          nvim_lua = '[api]',
+          path = '[path]',
+          luasnip = '[snip]',
+        },
+      }),
+    },
+    experimental = {
+      native_menu = false,
+
+      ghost_text = true,
+    },
+  })
+end
+
+return config
---
+++ lua/modules/completion/plugins.lua
@@ -0,0 +1,17 @@
+local plugin = require('core.pack').register_plugin
+local conf = require('modules.completion.config')
+
+plugin({
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lua',
+    'L3MON4D3/LuaSnip',
+    'onsails/lspkind.nvim',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = conf.cmp,
+})
