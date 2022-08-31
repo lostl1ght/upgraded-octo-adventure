dcd266adf62b88712b14f66d11e82deb7a383662
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-08-31 04:28:17 +0300

    Add fixcursorhold

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -3,4 +3,6 @@ local conf = require('modules.editor.config')
 
 plugin({ 'lewis6991/impatient.nvim' })
 
-plugin({ 'nvim-treesitter/nvim-treesitter', config = conf.treesitter})
+plugin({ 'nvim-treesitter/nvim-treesitter', config = conf.treesitter })
+
+plugin({ 'antoinemadec/FixCursorHold.nvim' }) -- Needed while issue https://github.com/neovim/neovim/issues/12587 is still open
