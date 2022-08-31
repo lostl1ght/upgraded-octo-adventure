16187c77057edf7f54dfa173d526d82f58c8a67b
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-01 02:35:53 +0300

    Add debugger

---
+++ lua/modules/debug/config.lua
@@ -0,0 +1,47 @@
+local config = {}
+
+function config.dap()
+  local dap = require('dap')
+  local dir = vim.fn.stdpath('data') .. '/codelldb/extension/'
+  local cmd = dir .. 'adapter/codelldb'
+  local lib = dir .. 'lldb/lib/liblldb.so'
+  dap.adapters.codelldb = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+      command = cmd,
+      args = { '--liblldb', lib, '--port', '${port}' },
+    },
+  }
+  dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = vim.fn.stdpath('data') .. '/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+  }
+
+  -- must be the last
+  vim.fn.sign_define('DapBreakpoint', { text = '⏺ ', texthl = 'ErrorMsg', linehl = '', numhl = '' })
+  vim.fn.sign_define('DapBreakpointRejected', { text = '⏺ ', texthl = 'ModeMsg', linehl = '', numhl = '' })
+  vim.fn.sign_define('DapStopped', { text = '⮕ ', texthl = 'Directory', linehl = '', numhl = '' })
+end
+
+function config.dapui()
+  local dapui = require('dapui')
+  local dap = require('dap')
+  dapui.setup()
+  dap.listeners.after.event_initialized['dapui_config'] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated['dapui_config'] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited['dapui_config'] = function()
+    dapui.close()
+  end
+
+  vim.api.nvim_create_user_command('DapEval', function()
+    dapui.eval()
+  end, { nargs = 0 })
+end
+
+return config
