b778d891e59c524381d18a9485a6cce75d70fce8
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-05 01:24:49 +0300

    Update lsp config

--- lua/modules/lsp/lspconfig.lua
+++ lua/modules/lsp/lspconfig.lua
@@ -65,21 +65,11 @@ function config.lua()
   require('lspconfig').sumneko_lua.setup({
     settings = {
       Lua = {
-        completion = {
-          autoRequire = false,
-        },
-        runtime = {
-          version = 'LuaJIT',
-        },
-        diagnostics = {
-          globals = { 'vim' },
-        },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file('', true),
-        },
-        telemetry = {
-          enable = false,
-        },
+        completion = { autoRequire = false, keywordSnippet = 'Disable' },
+        runtime = { version = 'LuaJIT' },
+        diagnostics = { globals = { 'vim' } },
+        workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+        telemetry = { enable = false },
       },
     },
     on_attach = function(client, bufnr)
@@ -92,63 +82,20 @@ function config.saga()
   local saga = require('lspsaga')
 
   saga.init_lsp_saga({
-    border_style = 'single',
-    saga_winblend = 0,
-    move_in_saga = { prev = '<C-p>', next = '<C-n>' },
     diagnostic_header = { ' ', ' ', ' ', ' ' },
-    show_diagnostic_source = true,
-    diagnostic_source_bracket = {},
-    max_preview_lines = 10,
     code_action_icon = '',
-    code_action_num_shortcut = true,
     code_action_lightbulb = {
-      enable = true,
       enable_in_insert = false,
-      cache_code_action = true,
       sign = false,
-      update_time = 150,
-      sign_priority = 20,
-      virtual_text = true,
     },
     finder_icons = {
-      def = '  ',
-      ref = '諭 ',
-      link = '  ',
+      def = '  ',
+      ref = '  ',
+      link = '  ',
     },
     finder_request_timeout = 3000,
-    finder_preview_hl_ns = 0,
     finder_action_keys = {
       open = '<enter>',
-      vsplit = 's',
-      split = 'i',
-      tabe = 't',
-      quit = 'q',
-      scroll_down = '<C-f>',
-      scroll_up = '<C-b>',
-    },
-    code_action_keys = {
-      quit = 'q',
-      exec = '<CR>',
-    },
-    rename_action_quit = '<C-c>',
-    rename_in_select = true,
-    definition_preview_icon = '  ',
-    symbol_in_winbar = {
-      in_custom = false,
-      enable = false,
-      separator = ' ',
-      show_file = true,
-      click_support = false,
-    },
-    show_outline = {
-      win_position = 'right',
-      win_with = '',
-      win_width = 30,
-      auto_enter = true,
-      auto_preview = true,
-      virt_text = '┃',
-      jump_key = 'o',
-      auto_refresh = true,
     },
   })
 end
