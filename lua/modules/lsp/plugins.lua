1f5e4cb5d5af2c0260623778231cb1d1a5ae8f89
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-07 20:13:38 +0300

    Lazy load lsp module

--- lua/modules/lsp/plugins.lua
+++ lua/modules/lsp/plugins.lua
@@ -1,16 +1,25 @@
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.lsp.config')
 
-plugin({ 'neovim/nvim-lspconfig', config = conf.lspconfig })
+plugin({ 'neovim/nvim-lspconfig', ft = { 'lua', 'rust', 'haskell' }, config = conf.lspconfig })
 
-plugin({ 'glepnir/lspsaga.nvim', branch = 'main', config = conf.lspsaga })
+plugin({ 'glepnir/lspsaga.nvim', cmd = 'Lspsaga', branch = 'main', config = conf.lspsaga })
 
-plugin({ 'jose-elias-alvarez/null-ls.nvim', requires = 'nvim-lua/plenary.nvim', config = conf.null_ls })
+plugin({
+  'jose-elias-alvarez/null-ls.nvim',
+  after = 'nvim-lspconfig',
+  requires = 'nvim-lua/plenary.nvim',
+  config = conf.null_ls,
+})
 
-plugin({ 'simrat39/symbols-outline.nvim', config = conf.outline })
+plugin({ 'simrat39/symbols-outline.nvim', cmd = 'SymbolsOutline', config = conf.outline })
 
-plugin({ 'folke/trouble.nvim', config = conf.trouble })
+plugin({ 'folke/trouble.nvim', cmd = 'TroubleToggle', config = conf.trouble })
 
-plugin({ 'ray-x/lsp_signature.nvim', config = conf.signature })
+plugin({ 'ray-x/lsp_signature.nvim', after = 'nvim-lspconfig', config = conf.signature })
 
-plugin({ "https://git.sr.ht/~whynothugo/lsp_lines.nvim", after = 'nvim-lspconfig', config = conf.lsplines, })
+plugin({
+  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  after = 'nvim-lspconfig',
+  config = conf.lsplines,
+})
