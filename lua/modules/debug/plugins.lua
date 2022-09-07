200ea951557a76ab373cadfdadc9af00e3f2caab
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-07 20:16:17 +0300

    Lazy load debug module

--- lua/modules/debug/plugins.lua
+++ lua/modules/debug/plugins.lua
@@ -1,8 +1,21 @@
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.debug.config')
 
-plugin({ 'mfussenegger/nvim-dap', config = conf.dap })
+plugin({
+  'mfussenegger/nvim-dap',
+  cmd = {
+    'DapContinue',
+    'DapTerminate',
+    'DapToggleBreakpoint',
+    'DapStepOver',
+    'DapStepInto',
+    'DapStepOut',
+    'DapEval',
+    'DapRun',
+  },
+  config = conf.dap,
+})
 
-plugin({ 'rcarriga/nvim-dap-ui', requires = 'mfussenegger/nvim-dap', config = conf.dapui })
+plugin({ 'rcarriga/nvim-dap-ui', requires = 'mfussenegger/nvim-dap', config = conf.dapui, after = 'nvim-dap' })
 
-plugin({ 'lostl1ght/dap-json.nvim', requires = 'mfussenegger/nvim-dap', config = conf.dapjson})
+plugin({ 'lostl1ght/dap-json.nvim', requires = 'mfussenegger/nvim-dap', config = conf.dapjson, after = 'nvim-dap' })
