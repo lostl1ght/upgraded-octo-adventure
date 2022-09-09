bf51d6352eecb4b6b77321c5f1dc50d6bedd7b24
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-09 03:46:09 +0300

    Improve cmp sequencing

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -1,20 +1,22 @@
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.completion.config')
 
+plugin({ 'onsails/lspkind.nvim', event = { 'InsertEnter', 'CmdLineEnter' } })
+plugin({ 'L3MON4D3/LuaSnip', config = conf.luasnip, event = { 'InsertEnter', 'CmdLineEnter' } })
+
 plugin({
   'hrsh7th/nvim-cmp',
   requires = {
     { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' },
-    { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
     { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' },
     { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
-    { 'onsails/lspkind.nvim', event = { 'InsertEnter', 'CmdLineEnter' } },
-    { 'L3MON4D3/LuaSnip', event = { 'InsertEnter', 'CmdLineEnter' } },
+    { 'hrsh7th/cmp-buffer' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'onsails/lspkind.nvim' },
   },
-  config = conf.cmp,
-  event = { 'InsertEnter', 'CmdLineEnter' },
+  after = { 'lspkind.nvim', 'LuaSnip' },
 })
 
-plugin({ 'L3MON4D3/LuaSnip', config = conf.luasnip, event = { 'InsertEnter', 'CmdLineEnter' }, after = 'nvim-cmp' })
+plugin({ 'hrsh7th/cmp-buffer', after = 'nvim-cmp', config = conf.cmp })
