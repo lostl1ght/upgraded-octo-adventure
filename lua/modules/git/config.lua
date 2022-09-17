3f4735d7e86d6a29936234005d93b53bfc13cf47
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-17 22:54:40 +0300

    Update lazygit config

--- lua/modules/git/config.lua
+++ lua/modules/git/config.lua
@@ -5,16 +5,7 @@ function config.gitsigns()
 end
 
 function config.lazygit()
-  require('lazygit').setup({
-    on_enter = function()
-      vim.keymap.del('t', 'ii', {})
-      vim.opt_local.sidescrolloff = 0
-      vim.opt_local.virtualedit = 'block'
-    end,
-    on_leave = function()
-      vim.keymap.set('t', 'ii', '<c-\\><c-n>', {})
-    end,
-  })
+  require('lazygit').setup()
 end
 
 return config
