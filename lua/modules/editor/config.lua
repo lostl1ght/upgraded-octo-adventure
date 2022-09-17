81693496c5d883b09ff5b792c5179f9221a95b43
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-09-18 01:14:35 +0300

    Remove cinnamon keymaps

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -39,6 +39,7 @@ function config.tabout()
       { open = '[', close = ']' },
       { open = '{', close = '}' },
       { open = '<', close = '>' },
+      { open = '$', close = '$' },
     },
     ignore_beginning = true,
   })
@@ -50,36 +51,18 @@ function config.cinnamon()
     scroll_limit = 75,
     default_delay = 4,
   })
-  -- Half-window movements:
-  vim.keymap.set({ 'n', 'x' }, '<C-u>', "<Cmd>lua Scroll('<C-u>', 1, 1)<CR>")
-  vim.keymap.set({ 'n', 'x' }, '<C-d>', "<Cmd>lua Scroll('<C-d>', 1, 1)<CR>")
-
-  -- Page movements:
-  vim.keymap.set({ 'n', 'x' }, '<C-b>', "<Cmd>lua Scroll('<C-b>', 1, 1)<CR>")
-  vim.keymap.set({ 'n', 'x' }, '<C-f>', "<Cmd>lua Scroll('<C-f>', 1, 1)<CR>")
-  vim.keymap.set({ 'n', 'x' }, '<PageUp>', "<Cmd>lua Scroll('<C-b>', 1, 1)<CR>")
-  vim.keymap.set({ 'n', 'x' }, '<PageDown>', "<Cmd>lua Scroll('<C-f>', 1, 1)<CR>")
-
   -- Start/end of file and line number movements:
-  vim.keymap.set({ 'n', 'x' }, 'gg', "<Cmd>lua Scroll('gg', 0, 1)<CR>")
-  vim.keymap.set({ 'n', 'x' }, 'G', "<Cmd>lua Scroll('G', 0, 1)<CR>")
-
-  -- Paragraph movements:
-  vim.keymap.set({ 'n', 'x' }, '{', "<Cmd>lua Scroll('{')<CR>")
-  vim.keymap.set({ 'n', 'x' }, '}', "<Cmd>lua Scroll('}')<CR>")
+  vim.keymap.set({ 'n', 'x' }, 'gg', "<Cmd>lua Scroll('gg', 0, 1)<CR>", {})
+  vim.keymap.set({ 'n', 'x' }, 'G', "<Cmd>lua Scroll('G', 0, 1)<CR>", {})
 
   -- Previous/next search result:
-  vim.keymap.set('n', 'n', "<Cmd>lua Scroll('n', 1)<CR>")
-  vim.keymap.set('n', 'N', "<Cmd>lua Scroll('N', 1)<CR>")
-
-  -- Previous/next cursor location:
-  vim.keymap.set('n', '<C-o>', "<Cmd>lua Scroll('<C-o>', 1)<CR>")
-  vim.keymap.set('n', '<C-i>', "<Cmd>lua Scroll('1<C-i>', 1)<CR>")
+  vim.keymap.set('n', 'n', "<Cmd>lua Scroll('n', 1)<CR>", {})
+  vim.keymap.set('n', 'N', "<Cmd>lua Scroll('N', 1)<CR>", {})
 
   -- Screen scrolling:
-  vim.keymap.set('n', 'zz', "<Cmd>lua Scroll('zz', 0, 1)<CR>")
-  vim.keymap.set('n', 'zt', "<Cmd>lua Scroll('zt', 0, 1)<CR>")
-  vim.keymap.set('n', 'zb', "<Cmd>lua Scroll('zb', 0, 1)<CR>")
+  vim.keymap.set('n', 'zz', "<Cmd>lua Scroll('zz', 0, 1)<CR>", {})
+  vim.keymap.set('n', 'zt', "<Cmd>lua Scroll('zt', 0, 1)<CR>", {})
+  vim.keymap.set('n', 'zb', "<Cmd>lua Scroll('zb', 0, 1)<CR>", {})
 end
 
 function config.surround()
