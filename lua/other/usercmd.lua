9cfca16a4105ed4e4993eef8c968490832b34e94
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-11-10 15:04:14 +0300

    Remove hydra usercmds

--- lua/other/usercmd.lua
+++ lua/other/usercmd.lua
@@ -1,19 +1,4 @@
-local cmds = {
-  {
-    'DapHydra',
-    function()
-      require('modules.ui.hydra').debug:activate()
-    end,
-    { nargs = 0 },
-  },
-  {
-    'SessionHydra',
-    function()
-      require('modules.ui.hydra').session:activate()
-    end,
-    { nargs = 0 },
-  },
-}
+local cmds = {}
 
 for _, v in ipairs(cmds) do
   vim.api.nvim_create_user_command(unpack(v))
