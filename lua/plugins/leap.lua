cd366009ecdf2f77b1b33d4d09bb3a9d7d63d2da
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-12-21 00:53:24 +0300

    Add leap extensions

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,6 +1,20 @@
-local M = {'ggandor/leap.nvim'}
+local M = { 'ggandor/leap.nvim' }
 
 M.event = 'VeryLazy'
 
+M.dependencies = {
+  { 'ggandor/flit.nvim' },
+  { 'ggandor/leap-ast.nvim' },
+}
+
+M.config = function()
+  require('leap').add_default_mappings()
+  require('flit').setup({
+    labeled_modes = 'nv',
+  })
+  vim.keymap.set({ 'n', 'x', 'o' }, 'M', function()
+    require('leap-ast').leap()
+  end, { desc = 'Leap ast' })
+end
 
 return M
