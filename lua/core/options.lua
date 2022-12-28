b068fb22b66300a4e652f4f4bf453075423cefe8
Contsantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-12-29 02:17:27 +0300

    Use vim.env variable

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -54,7 +54,7 @@ vim.opt.cmdheight = 0
 vim.opt.timeoutlen = 500
 vim.opt.shortmess:append('I')
 vim.g.tex_flavor = 'latex'
-vim.g.python3_host_prog = os.getenv('HOME') .. '/.pyenv/versions/neovim/bin/python3'
+vim.g.python3_host_prog = vim.env.HOME .. '/.pyenv/versions/neovim/bin/python3'
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
